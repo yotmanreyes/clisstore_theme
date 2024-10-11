1dfc38a8d2529c68b0b6289f18bf0a6ace14b55f
yotmanreyes <yotman.reyes@gmail.com>
2024-10-11 18:54:35 -0400

    update app

--- js/app.js
+++ js/app.js
@@ -2,9 +2,21 @@
   let openMenuTrigger = document.querySelector(".hamburger-icon");
   let closeMenuTrigger = document.querySelector(".close-icon");
   let menu = document.querySelector(".nav-menu");
+  let searchContainer = document.querySelector(".search-container");
+  let shopCartContainer = document.querySelector(".shopcart-container");
+  let searchBoxTrigger = document.querySelector(".search-box-trigger");
+  let shopCartTrigger = document.querySelector(".shopcart-container");
 
   openMenuTrigger.addEventListener("click", function () {
     document.body.classList.toggle("fixed-height");
     menu.classList.toggle("is-show");
+  });
+
+  searchBoxTrigger.addEventListener("click", function () {
+    searchContainer.classList.add("is-show");
+  });
+
+  shopCartTrigger.addEventListener("click", function () {
+    shopCartContainer.classList.add("is-show");
   });
 })();
