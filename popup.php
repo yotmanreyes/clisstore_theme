0caf90d937f3346b797d54e64df6eb6b06905b7f
yotmanreyes <yotman.reyes@gmail.com>
2024-10-13 19:12:51 -0400

    update app

--- popup.php
+++ popup.php
@@ -1,8 +1,8 @@
 <div class="popup">
-    <div>
+    <div class="section">
         <img src="<?php echo get_theme_mod('popup_background_image') ?>" alt="">
     </div>
-    <div>
+    <div class="section">
         <?php the_custom_logo(); ?>
         <h2><?php echo get_theme_mod('popup-title') ?></h2>
         <p><?php echo get_theme_mod('popup-description') ?></p>
