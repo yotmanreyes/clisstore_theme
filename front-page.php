cb4b14dcc8d09772a28720002bd24538397aabe5
yotmanreyes <yotman.reyes@gmail.com>
2024-10-13 10:40:48 -0400

    update app

--- front-page.php
+++ front-page.php
@@ -260,11 +260,11 @@ get_header();
         </div>
     </aside>
     
-    <a href="<?php echo get_theme_mod('whatsapp-url') ?>" class="whatsapp-icon">
+    <a href="<?php echo get_theme_mod('whatsapp_number') ?>" class="whatsapp-icon">
         <img src="<?php echo get_template_directory_uri() . '/images/wa-icon.svg' ?>" alt="">
     </a>
     
-    <a href="<?php echo get_theme_mod('instagram-url') ?>" class="instagram-icon">
+    <a href="<?php echo get_theme_mod('instagram_url') ?>" class="instagram-icon">
         <img src="<?php echo get_template_directory_uri() . '/images/instagram-icon.svg' ?>" alt="">
     </a>
 <?php
