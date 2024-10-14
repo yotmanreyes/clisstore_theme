49b109d6bb9c935c815482547898c047f1cb5f39
yotmanreyes <yotman.reyes@gmail.com>
2024-10-13 23:16:51 -0400

    update app

--- popup.php
+++ popup.php
@@ -6,5 +6,15 @@
         <?php the_custom_logo(); ?>
         <h2><?php echo get_theme_mod('popup-title') ?></h2>
         <p><?php echo get_theme_mod('popup-description') ?></p>
+
+        <form action="">
+            <div class="form-inline">
+                <input type="text" class="form-control" name="">
+                <input type="text" class="form-control" name="">
+            </div>
+            <div class="form-field">
+                <input type="email" name="" id="">
+            </div>
+        </form>
     </div>
 </div>
