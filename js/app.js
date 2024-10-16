5ba90f51bb97afda8d52b591297d89d3df5c6794
yotmanreyes <yotman.reyes@gmail.com>
2024-10-16 00:56:03 -0400

    update app

--- js/app.js
+++ js/app.js
@@ -42,7 +42,11 @@
   });
 
   setTimeout(function () {
-    showPopup();
+    // Function to check if the popup should be shown
+    if (!getCookie("popupDisplayed")) {
+      showPopup();
+      setCookie("popupDisplayed", "true", 30); // Set cookie for 30 days
+    }
   }, 5000);
 
   function showPopup() {
@@ -53,5 +57,28 @@
   function closePopUp() {
     popup.classList.remove("is-show");
     backdrop.classList.remove("is-show");
+  }
+
+  // Function to set a cookie
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  // Function to get a cookie
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
   }
 })();
