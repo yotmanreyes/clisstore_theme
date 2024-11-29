6d28ef7c7bbcbb6786c5a124b9c7453efc03eb1e
yotmanreyes <yotman.reyes@gmail.com>
2024-11-29 01:04:08 -0400

    update app

--- archive-product.php
+++ archive-product.php
@@ -2,8 +2,6 @@
 /**
  * The Template for displaying product archives, including the main shop page which is a post type archive
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
  * @version 3.4.0
@@ -15,10 +13,6 @@ get_header( 'shop' );
 
 /**
  * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
  */
 do_action( 'woocommerce_before_main_content' );
 
@@ -31,9 +25,6 @@ do_action( 'woocommerce_before_main_content' );
     <?php
     /**
      * Hook: woocommerce_archive_description.
-     *
-     * @hooked woocommerce_taxonomy_archive_description - 10
-     * @hooked woocommerce_product_archive_description - 10
      */
     do_action( 'woocommerce_archive_description' );
     ?>
@@ -43,14 +34,14 @@ if ( woocommerce_product_loop() ) {
 
     /**
      * Hook: woocommerce_before_shop_loop.
-     *
-     * @hooked woocommerce_output_all_notices - 10
-     * @hooked woocommerce_result_count - 20
-     * @hooked woocommerce_catalog_ordering - 30
      */
     do_action( 'woocommerce_before_shop_loop' );
 
     woocommerce_product_loop_start();
+
+    ?>
+    <div class="products-grid">
+    <?php
 
     if ( wc_get_loop_prop( 'total' ) ) {
         while ( have_posts() ) {
@@ -63,72 +54,70 @@ if ( woocommerce_product_loop() ) {
 
             global $product;
             ?>
-            <div class="product-image-slide">
-                <a class="link" href="<?php echo esc_url(get_the_permalink()); ?>">
-                    <?php
-                    // Mostrar la imagen destacada
-                    if ( has_post_thumbnail() ) {
-                        the_post_thumbnail('full', array('class' => 'main-image'));
-                    }
-
-                    // Mostrar galería de imágenes
-                    $attachment_ids = $product->get_gallery_image_ids();
-                    if ( $attachment_ids ) {
-                        echo '<div class="product-gallery">';
-                        foreach( $attachment_ids as $attachment_id ) {
-                            echo wp_get_attachment_image( $attachment_id, 'thumbnail', false, array('class' => 'gallery-image') );
+            <div class="product-grid-item">
+                <div class="product-image-slide">
+                    <a class="link" href="<?php echo esc_url(get_the_permalink()); ?>">
+                        <?php
+                        // Mostrar la imagen destacada
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail('full', array('class' => 'main-image'));
                         }
-                        echo '</div>';
-                    }
 
-                    // Mostrar atributos (tallas)
-                    if ( $product->get_attribute( 'size' ) ) {
-                        $sizes = explode(', ', $product->get_attribute( 'size' ));
-                        echo '<ul class="product-sizes">';
-                        foreach($sizes as $size){
-                            echo '<li class="size">' . esc_html($size) . '</li>'; 
+                        // Mostrar galería de imágenes
+                        $attachment_ids = $product->get_gallery_image_ids();
+                        if ( $attachment_ids ) {
+                            echo '<div class="product-gallery">';
+                            foreach( $attachment_ids as $attachment_id ) {
+                                echo wp_get_attachment_image( $attachment_id, 'thumbnail', false, array('class' => 'gallery-image') );
+                            }
+                            echo '</div>';
                         }
-                        echo '</ul>';
-                    }
-                    ?>
-                    <div class="product-description">
-                        <h4><?php the_title(); ?></h4>
-                        <p><?php echo $product->get_price_html(); ?></p>
-                    </div>
-                </a>
+
+                        // Mostrar atributos (tallas)
+                        if ( $product->get_attribute( 'size' ) ) {
+                            $sizes = explode(', ', $product->get_attribute( 'size' ));
+                            echo '<ul class="product-sizes">';
+                            foreach($sizes as $size){
+                                echo '<li class="size">' . esc_html($size) . '</li>'; 
+                            }
+                            echo '</ul>';
+                        }
+                        ?>
+                        <div class="product-description">
+                            <h4><?php the_title(); ?></h4>
+                            <p><?php echo $product->get_price_html(); ?></p>
+                        </div>
+                    </a>
+                </div>
             </div>
             <?php
         }
     }
 
+    ?>
+    </div>
+    <?php
+
     woocommerce_product_loop_end();
 
     /**
      * Hook: woocommerce_after_shop_loop.
-     *
-     * @hooked woocommerce_pagination - 10
      */
     do_action( 'woocommerce_after_shop_loop' );
 } else {
     /**
      * Hook: woocommerce_no_products_found.
-     *
-     * @hooked wc_no_products_found - 10
      */
     do_action( 'woocommerce_no_products_found' );
 }
 
 /**
  * Hook: woocommerce_after_main_content.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
 do_action( 'woocommerce_after_main_content' );
 
 /**
  * Hook: woocommerce_sidebar.
- *
- * @hooked woocommerce_get_sidebar - 10
  */
 do_action( 'woocommerce_sidebar' );
 
